941ea36d6e6d8648ad1e688f4de6eaac514f5c3c
Odin Jacques <alexander.jacques02@gmail.com>
2025-04-27 14:32:18 -0600

    Added Favorite view and Navigation
    Added Favorite view and Navigation

---
+++ src/views/PokemonByFavorite.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { FavoritePokemon } from "../components/FavoritePokemon/FavoritePokemon";
+
+
+const PokemonByFavorite: React.FC = () => <FavoritePokemon/>;
+
+export default PokemonByFavorite;
